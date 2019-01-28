0943de1778eb7ca830c67dad86a2af1b6d120123
joney000 <developer.jaswant@gmail.com>
2019-01-29 00:53:30 +0530

    init binary classification class

---
+++ Classification/BinaryClassification.py
@@ -0,0 +1,18 @@
+import sys
+import numpy as np
+
+class BinaryClassification:
+
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+class Preprocessor:
+    @staticmethod
+    def process(data):
+        print("in the data")
+
+def main():
+    model = BinaryClassification(1.7)
+    print(model.learning_rate)
+
+if  __name__ =='__main__':main()
