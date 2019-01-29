81fbbc4846bb58f8e8409176030fe2dd5536374a
joney000 <developer.jaswant@gmail.com>
2019-01-30 01:02:01 +0530

    add template for preprocessing

--- Classification/BinaryClassification.py
+++ Classification/BinaryClassification.py
@@ -1,18 +1,24 @@
 import sys
 import numpy as np
+import Globals.Preprocessor
+import math
 
+"""
+author: Jaswant Singh [developer.jaswant@gmail.com]
+
+"""
 class BinaryClassification:
-
-    def __init__(self, learning_rate):
+    w = [] *
+    def __init__(self, learning_rate, num_of_training_examples, num_of_test_examples, num_of_features):
         self.learning_rate = learning_rate
+        
+    def sigmoid(self, x):
+        return 1./(1. + math.exp(-x))
 
-class Preprocessor:
-    @staticmethod
-    def process(data):
-        print("in the data")
+    def predic(self, y):
+        return sigmoid(w)
 
 def main():
-    model = BinaryClassification(1.7)
+    model = BinaryClassification(1.7)   #
     print(model.learning_rate)
-
 if  __name__ =='__main__':main()
---
+++ Globals/Preprocessor.py
@@ -0,0 +1,9 @@
+"""
+author: Jaswant Singh [developer.jaswant@gmail.com]
+desc  : contains a basic data preprocessing utilities like normalization, structuring, filtering, Completing missing data.
+"""
+
+class Preprocessor:
+    @staticmethod
+    def process(data):
+        print("in the data")
