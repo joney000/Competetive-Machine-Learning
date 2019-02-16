4a8a7f60457945b3687ff603fd002d77826c554c
joney000 <developer.jaswant@gmail.com>
2019-02-16 20:32:33 +0530

    add example to functional programming and function vectorization in numpy

--- Classification/BinaryClassification.py
+++ Classification/BinaryClassification.py
@@ -1,24 +1,50 @@
 import sys
+import time
 import numpy as np
-import Globals.Preprocessor
-import math
+from Globals.FileParser import FileParser
+from Globals.Preprocessor import Preprocessor
 
 """
-author: Jaswant Singh [developer.jaswant@gmail.com]
+author:    Jaswant Singh [developer.jaswant@gmail.com]
+algorithm: logistic regression
+domain:    classification
 
 """
 class BinaryClassification:
-    w = [] *
-    def __init__(self, learning_rate, num_of_training_examples, num_of_test_examples, num_of_features):
-        self.learning_rate = learning_rate
-        
-    def sigmoid(self, x):
-        return 1./(1. + math.exp(-x))
 
-    def predic(self, y):
-        return sigmoid(w)
+    def __init__(self, learning_rate, num_of_training_examples, num_of_test_examples, num_of_features, train_data, test_data):
+        self.learning_rate = learning_rate
+        self.weight = np.zeros((1, num_of_features)) # 1 * N
+        self.num_of_training_examples = num_of_training_examples
+        self.num_of_test_examples = num_of_test_examples
+        self.test_data = test_data
+        self.train_data = train_data
+
+    def sigmoid(x):
+        return 1./(1. + np.exp(-x))
+
+    def predic(self, x):        # the y^
+        p = 1
+
+
+    def train(self):
+        train_input_x  = self.train_data[0]
+        train_output_y = self.train_data[1]
+        z = np.dot(train_input_x, self.weight.T)  # z = train_input_x [M * N] dot w^T [N * 1] = z[M * 1]
+        vectorized_sigmoid = np.vectorize(self.sigmoid)
+        z = vectorized_sigmoid(z)   # z[i] = sigmoid(z[i])
+    def test(self):
+        q = 1#
 
 def main():
-    model = BinaryClassification(1.7)   #
+    start = time.time()
+    data = FileParser.readFile("Data\")
+
+    # data = Preprocessor.process(data)
+    model = BinaryClassification(1.7, 1, 1, 1)   #
     print(model.learning_rate)
+    model.train()
+    end = time.time()
+    print("Time taken vector: "+(str)((end - start) * 100) +"ms")
+
 if  __name__ =='__main__':main()
